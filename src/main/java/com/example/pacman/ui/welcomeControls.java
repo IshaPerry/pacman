4e46082a6d842c8c62466e465dd1006170bc14d6
Lee, Suemin <slee3363@gatech.edu>
2022-10-11 23:06:31 -0400

    Method added to welcomeControls that allows for scene changes!

--- src/main/java/com/example/pacman/ui/welcomeControls.java
+++ src/main/java/com/example/pacman/ui/welcomeControls.java
@@ -1,8 +1,12 @@
 package com.example.pacman.ui;
 
+import javafx.animation.FadeTransition;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
@@ -10,9 +14,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.FXML;
+import javafx.util.Duration;
+
 import java.io.FileNotFoundException;
-
-
+import java.io.IOException;
 
 
 public class welcomeControls {
@@ -24,19 +29,21 @@ public class welcomeControls {
     @FXML
     private ImageView image;
 
+    public Stage stage;
+
     @FXML
     public void initialize() throws FileNotFoundException {
         //startButton
         start.setOnAction(e -> {
-            //Stage stage = (Stage) start.getScene().getWindow();
-           // stage.close();
+            Stage stage = (Stage) start.getScene().getWindow();
+            this.stage = stage;
+            changeScene(stage, "/InfoScreen.fxml");
         });
 
         //exitButton
         exit.setOnAction(e -> {
-           // System.exit(0);
+            stage.close();
         });
-
 
         //imageView
        ImageView image = new ImageView("/Pac-Man-Ghosts.jpeg");
@@ -51,6 +58,24 @@ public class welcomeControls {
          */
 
 
+    }
+
+    public void changeScene(Stage stage, String filePath) {
+        try {
+            this.stage = stage;
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(filePath));
+            // Scene newScene = new Scene(fxmlLoader.load(), 600,400);
+            AnchorPane page = (AnchorPane) fxmlLoader.load();
+            FadeTransition ft = new FadeTransition(Duration.millis(3000), page);
+            ft.setFromValue(0.0);
+            ft.setToValue(1.0);
+            ft.play();
+            Scene newScene = new Scene(page);
+            this.stage.setScene(newScene);
+            throw new IOException();
+        } catch (IOException e) {
+            System.out.println("");
+        }
     }
 }
 
