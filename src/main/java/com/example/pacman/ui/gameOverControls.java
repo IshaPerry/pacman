dd5ca4f75179d1f081c7bd5a1aa2b834ed8df357
Lee, Suemin <slee3363@gatech.edu>
2022-11-21 17:36:16 -0500

    Updates to game over screen

--- src/main/java/com/example/pacman/ui/gameOverControls.java
+++ src/main/java/com/example/pacman/ui/gameOverControls.java
@@ -26,9 +26,9 @@ import java.util.Objects;
 
 public class gameOverControls {
     private static Stage stage;
-
-    @FXML
     private static Label gameOverLabel = new Label();
+    private static Label scoreLabel = new Label();
+    private static Label livesLabel = new Label();
 
 
     @FXML
@@ -46,28 +46,39 @@ public class gameOverControls {
         System.exit(0);
     }
 
-
-
-    public static void changeScene(Stage newStage, URL location, String text) {
-        try {
-            stage = newStage;
-            stage.setTitle("Game Over");
-            FXMLLoader fxmlLoader = new FXMLLoader(location);
-            // Scene newScene = new Scene(fxmlLoader.load(), 600,400);
-            AnchorPane page = (AnchorPane) fxmlLoader.load();
-            Scene newScene = new Scene(page);
-
-            gameOverLabel.setText(text);
-            page.getChildren().add(gameOverLabel);
-            gameOverLabel.setFont(Font.font("Ayuthaya", 20));
-            gameOverLabel.setTextFill(Color.WHITE);
-            gameOverLabel.setTranslateX(230);
-            gameOverLabel.setTranslateY(230);
-            stage.setScene(newScene);
-        } catch (IOException e) {
-            System.out.println(e.toString());
-        }
-
-
+    @FXML
+    public static void changeScene(Stage currentStage, String text, int lives, int score) throws NullPointerException, IOException {
+        URL location = gameOverControls.class.getResource("/gameOver.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(location);
+        AnchorPane page = (AnchorPane) fxmlLoader.load();
+        Scene newScene = new Scene(page);
+        currentStage.setTitle("Game Over");
+        page.getChildren().add(gameOverLabel);
+        page.getChildren().add(livesLabel);
+        page.getChildren().add(scoreLabel);
+        livesLabel.setMinHeight(29);
+        livesLabel.setMinWidth(151);
+        livesLabel.setTranslateX(317);
+        livesLabel.setTranslateY(197);
+        livesLabel.setText(String.valueOf(lives));
+        scoreLabel.setMinHeight(29);
+        scoreLabel.setMinWidth(151);
+        scoreLabel.setTranslateX(317);
+        scoreLabel.setTranslateY(226);
+        scoreLabel.setText(String.valueOf(score));
+        gameOverLabel.setMinHeight(40);
+        gameOverLabel.setMinWidth(111);
+        gameOverLabel.setTranslateX(262);
+        gameOverLabel.setTranslateY(157);
+        gameOverLabel.setTextFill(Color.WHITE);
+        gameOverLabel.setText(text);
+        gameOverLabel.setFont(Font.font("Ayuthaya", 20));
+        livesLabel.setFont(Font.font("Ayuthaya", 20));
+        livesLabel.setTextFill(Color.WHITE);
+        scoreLabel.setFont(Font.font("Ayuthaya", 20));
+        scoreLabel.setTextFill(Color.WHITE);
+        currentStage.setScene(newScene);
+        currentStage.setTitle("Game over");
     }
+
 }
