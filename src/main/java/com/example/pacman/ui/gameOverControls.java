ec8c4267e3dff22849516067b3de86f0fe8af64b
Wu, Emily Jaling <ewu64@gatech.edu>
2022-11-28 20:14:48 -0500

    changed win and lose screen labels

--- src/main/java/com/example/pacman/ui/gameOverControls.java
+++ src/main/java/com/example/pacman/ui/gameOverControls.java
@@ -6,29 +6,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
-
 import java.io.IOException;
 import java.net.URL;
-import java.util.EventObject;
 import java.util.Objects;
 
 
 public class gameOverControls {
     private static Stage stage;
-    private static Label gameOverLabel = new Label();
     private static Label scoreLabel = new Label();
     private static Label livesLabel = new Label();
+    private static Label ghostsLabel = new Label();
+    private static Image win = new Image(Objects.requireNonNull(gameOverControls.class.getResourceAsStream("/winScreen.png")));
+    private static Image lose = new Image(Objects.requireNonNull(gameOverControls.class.getResourceAsStream("/loseScreen.png")));
 
 
     @FXML
@@ -37,7 +33,9 @@ public class gameOverControls {
         Parent root = loader.load();
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
+
         stage.setScene(scene);
+
         stage.show();
     }
 
@@ -47,38 +45,40 @@ public class gameOverControls {
     }
 
     @FXML
-    public static void changeScene(Stage currentStage, String text, int lives, int score) throws NullPointerException, IOException {
+    public static void changeScene(Stage currentStage, int lives, int score, int ghostsEaten) throws NullPointerException, IOException {
         URL location = gameOverControls.class.getResource("/gameOver.fxml");
+
         FXMLLoader fxmlLoader = new FXMLLoader(location);
         AnchorPane page = (AnchorPane) fxmlLoader.load();
         Scene newScene = new Scene(page);
+        ImageView background = (ImageView) newScene.lookup("#background");
+        if (GameModel.getGameStatus() == GameModel.GameState.LOSE) {
+            background.setImage(lose);
+        } else {
+            background.setImage(win);
+        }
+
         currentStage.setTitle("Game Over");
-        page.getChildren().add(gameOverLabel);
         page.getChildren().add(livesLabel);
         page.getChildren().add(scoreLabel);
-        livesLabel.setMinHeight(29);
-        livesLabel.setMinWidth(151);
-        livesLabel.setTranslateX(317);
-        livesLabel.setTranslateY(197);
+        page.getChildren().add(ghostsLabel);
+
+        livesLabel.setTranslateX(305);
+        livesLabel.setTranslateY(178);
         livesLabel.setText(String.valueOf(lives));
-        scoreLabel.setMinHeight(29);
-        scoreLabel.setMinWidth(151);
-        scoreLabel.setTranslateX(317);
-        scoreLabel.setTranslateY(226);
-        scoreLabel.setText(String.valueOf(score));
-        gameOverLabel.setMinHeight(40);
-        gameOverLabel.setMinWidth(111);
-        gameOverLabel.setTranslateX(262);
-        gameOverLabel.setTranslateY(157);
-        gameOverLabel.setTextFill(Color.WHITE);
-        gameOverLabel.setText(text);
-        gameOverLabel.setFont(Font.font("Ayuthaya", 20));
         livesLabel.setFont(Font.font("Ayuthaya", 20));
-        livesLabel.setTextFill(Color.WHITE);
+        livesLabel.setTextFill(Color.BLACK);
+        scoreLabel.setTranslateX(305);
+        scoreLabel.setTranslateY(211);
+        scoreLabel.setText(String.valueOf(score));
         scoreLabel.setFont(Font.font("Ayuthaya", 20));
-        scoreLabel.setTextFill(Color.WHITE);
+        scoreLabel.setTextFill(Color.BLACK);
+        ghostsLabel.setTranslateX(305);
+        ghostsLabel.setTranslateY(245);
+        ghostsLabel.setText(String.valueOf(ghostsEaten));
+        ghostsLabel.setFont(Font.font("Ayuthaya", 20));
+        ghostsLabel.setTextFill(Color.BLACK);
         currentStage.setScene(newScene);
         currentStage.setTitle("Game over");
     }
-
 }
