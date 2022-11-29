c187a06aec598f094fcd29aa751175a2ce05bd5a
Chung, Audrey <achung63@gatech.edu>
2022-11-28 21:56:54 -0500

    font

--- src/main/java/com/example/pacman/ui/configurationControls.java
+++ src/main/java/com/example/pacman/ui/configurationControls.java
@@ -73,7 +73,7 @@ public class configurationControls {
         if (events == 0) {
             if (isSetUpValid()) {
                 userLabel2.setText(name + " is ready to play level " + level + " with character: " + pacmanColor + "!");
-                userLabel2.setStyle("-fx-font-size: 10;");
+                userLabel2.setStyle("-fx-font-size: 8;");
                 setLevelParams(level);
                 submitButton.setText("Continue");
                 events += 1;
